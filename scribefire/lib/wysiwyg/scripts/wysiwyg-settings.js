029220ebe532d2dfedebcdd979e2d90a0aeebf9a
cfinke <cfinke@a6530c7b-87e9-184b-5ef4-16c15c296bb4>
2010-04-16 06:39:46 +0000

    WYSIWYG editor

---
+++ scribefire/lib/wysiwyg/scripts/wysiwyg-settings.js
@@ -0,0 +1,25 @@
+var wysiwyg_scribefire = new WYSIWYG.Settings();
+wysiwyg_scribefire.ImagesDir = "lib/wysiwyg/images/";
+wysiwyg_scribefire.PopupsDir = "lib/wysiwyg/popups/";
+wysiwyg_scribefire.CSSFile = "lib/wysiwyg/styles/wysiwyg.css";
+wysiwyg_scribefire.Width = "100%"; 
+wysiwyg_scribefire.Height = "200px";
+
+wysiwyg_scribefire.Toolbar[0] = [
+		"viewSource", /* "preview", */
+	"seperator", 
+		"headings",
+		"bold", "italic", "underline", "strikethrough", "subscript", "superscript", "forecolor", 
+	"seperator", 
+		"createlink", /*"insertimage",*/ "inserttable",
+	"seperator",
+		"justifyleft", "justifycenter", "justifyright", 
+	"seperator", 
+		"unorderedlist", "orderedlist",
+	"seperator",
+		"outdent", "indent"
+		 /*, "maximize" */
+	 ];
+wysiwyg_scribefire.Toolbar[1] = [];
+wysiwyg_scribefire.Toolbar[2] = [];
+wysiwyg_scribefire.StatusBarEnabled = false;
