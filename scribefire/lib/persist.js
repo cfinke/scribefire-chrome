5d4d0289ecff8d246e404a2db7c1cbe58b1394c4
cfinke <cfinke@a6530c7b-87e9-184b-5ef4-16c15c296bb4>
2011-06-29 01:57:32 +0000

    Better persistence handling code

--- scribefire/lib/persist.js
+++ scribefire/lib/persist.js
@@ -1,44 +1,45 @@
 jQuery.fn.persist = function (property) {
 	return this.each(function () {
-		if (platform == 'gecko') {
-			// @see https://bugzilla.mozilla.org/show_bug.cgi?id=495747
-			var id = $(this).attr("id");
-
-			var persistence = SCRIBEFIRE.prefs.getJSONPref("persist", {});
+		var id = $(this).attr("id");
+		
+		if (id) {
+			if (platform == 'gecko') {
+				// @see https://bugzilla.mozilla.org/show_bug.cgi?id=495747
+				var persistence = SCRIBEFIRE.prefs.getJSONPref("persist", {});
 			
+				if (!(id in persistence)) {
+					persistence[id] = {};
+				}
+			
+				persistence[id][property] = $(this).attr(property);
+			
+				SCRIBEFIRE.prefs.setJSONPref("persist", persistence);
+			
+				return;
+			}
+			
+			var url = document.location.href;
+		
+			var persistenceString = localStorage["persist"];
+
+			var persistence = {};
+
+			if (persistenceString) {
+				try {
+					persistence = JSON.parse(persistenceString);
+				} catch (e) {
+					persistence = {};
+				}
+			}
+	
 			if (!(id in persistence)) {
 				persistence[id] = {};
 			}
-			
+	
 			persistence[id][property] = $(this).attr(property);
-			
-			SCRIBEFIRE.prefs.setJSONPref("persist", persistence);
-			
-			return;
-		}
-		
-		var url = document.location.href;
-		var id = $(this).attr("id");
-		
-		var persistenceString = localStorage["persist"];
-
-		var persistence = {};
-
-		if (persistenceString) {
-			try {
-				persistence = JSON.parse(persistenceString);
-			} catch (e) {
-				persistence = {};
-			}
-		}
 	
-		if (!(id in persistence)) {
-			persistence[id] = {};
+			localStorage["persist"] = JSON.stringify(persistence);
 		}
-	
-		persistence[id][property] = $(this).attr(property);
-	
-		localStorage["persist"] = JSON.stringify(persistence);
 	});
 };
 
